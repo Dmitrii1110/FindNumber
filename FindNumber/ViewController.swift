c3a96476fb968bd91492997ad9ed024346af966f
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2021-12-26 11:38:45 +0300

    Initial Commit

---
+++ FindNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FindNumber
+//
+//  Created by admin1 on 26.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
