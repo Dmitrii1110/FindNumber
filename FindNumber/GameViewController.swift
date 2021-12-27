fb14faa89d5bb930394d73b5395487e8d5421cea
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2021-12-27 17:58:39 +0300

    Compile lesson 5.1

--- FindNumber/GameViewController.swift
+++ FindNumber/GameViewController.swift
@@ -8,14 +8,48 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var nextDigit: UILabel!
     @IBOutlet var buttons: [UIButton]!
+    
+    lazy var game = Game(countItems: buttons.count)
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupScreen()
         
     }
 
     @IBAction func pressButton(_ sender: UIButton) {
-        sender.isHidden = true
-        print(sender.currentTitle)
+        
+        guard let buttonIndex = buttons.firstIndex(of: sender) else {return}
+        game.check(index:buttonIndex)
+        
+        updateUI()
     }
+    
+    private func setupScreen(){
+        
+        for index in game.items.indices{
+            buttons[index].setTitle(game.items[index].title, for: .normal) //сменили цифры на кнопки
+            buttons[index].isHidden = false //чтобы все кнопки были видны
+            
+            
+        }
+        
+        nextDigit.text = game.nextItem?.title
+    }
+    
+    private func updateUI(){
+        for index in game.items.indices{
+            buttons[index].isHidden = game.items[index].isFound
+        }
+        nextDigit.text = game.nextItem?.title
+        
+        if game.status == .win{
+            statusLabel.text = "Вы выиграли"
+            statusLabel.textColor = .green
+        }
+    }
+    
 }
