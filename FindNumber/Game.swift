9dd5a22305baa218aad3ce7dbfb4476685081d16
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2021-12-28 18:41:57 +0300

    Start customise UI

--- FindNumber/Game.swift
+++ FindNumber/Game.swift
@@ -13,6 +13,7 @@ import UIKit
 enum StatusGame{
     case start
     case win
+    case lose
 }
 
 class Game{
@@ -20,6 +21,7 @@ class Game{
     struct Item{
         var title:String
         var isFound:Bool = false
+        var isError = false
     }
     
     private let data = Array(1...99)
@@ -30,36 +32,93 @@ class Game{
     
     var nextItem: Item?
     
-    var status:StatusGame = .start
+    var status:StatusGame = .start{
+        didSet{
+            if status != .start{
+                stopGame()
+            }
+        }
+    }
     
-    init(countItems:Int){
+    private var timeForGame:Int
+    
+    private var secondsGame:Int{
+        didSet{
+            if secondsGame == 0{
+                status = .lose
+            }
+            updateTimer(status,secondsGame)
+        }
+    }
+    
+    private var timer:Timer?
+    private var updateTimer:((StatusGame, Int)->Void)
+    
+    init(countItems:Int,time:Int, updateTimer:@escaping (_ status:StatusGame,_ seconds:Int)->Void){
         self.countItems = countItems
+        self.timeForGame = time
+        self.secondsGame = time
+        self.updateTimer = updateTimer
         setupGame()
     }
     
     private func setupGame(){
         var digits = data.shuffled() //перемешиваем массив
         
+        items.removeAll()
+        
         while items.count < countItems{
             let item = Item(title: String(digits.removeFirst()))
             items.append(item)
+            
         }
         
         nextItem = items.shuffled().first //чтобы рандомно брать цифру
+        
+        //updateTimer(status,timeForGame)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block:{ [weak self] (_) in self?.secondsGame -= 1
+    })
+        
+    }
+    
+    func newGame(){
+        status = .start
+        setupGame()
+        self.secondsGame = self.timeForGame
+        
     }
     
     func check(index:Int){
-        
+        guard status == .start else {return}
         if items[index].title == nextItem?.title{
             items[index].isFound = true
-            nextItem = items.shuffled().first(where: { (item) -> Bool in
+            nextItem = items.shuffled().first(where: {(item) -> Bool in
                 item.isFound == false
         })
-    }
+        } else {
+            items[index].isError = true
+        }
         
         if nextItem == nil{
             status = .win
         }
+    }
+    
+    private func stopGame(){
+        timer?.invalidate()
+    }
+    
+}
+    
+    
+    extension Int{
+        func secondsToString()->String{
+            let minutes = self / 60
+            let seconds = self % 60
+            
+            return String(format: "%d:%02d", minutes, seconds)
+        
     }
     
 }
