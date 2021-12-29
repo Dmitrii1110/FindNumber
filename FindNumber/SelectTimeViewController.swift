7ebf3ac27b4f548898d4455b8796f4c8341c2fc7
Dmitrii_IT <74114011+Dmitrii1110@users.noreply.github.com>
2021-12-29 22:07:54 +0300

    lesson8. Перед заполнением   UITableView

---
+++ FindNumber/SelectTimeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SelectTimeViewController.swift
+//  FindNumber
+//
+//  Created by admin1 on 29.12.2021.
+//
+
+import UIKit
+
+class SelectTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+}
---
+++ FindNumber/SettingsTableTableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsTableTableViewController.swift
+//  FindNumber
+//
+//  Created by admin1 on 29.12.2021.
+//
+
+import UIKit
+
+class SettingsTableTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
